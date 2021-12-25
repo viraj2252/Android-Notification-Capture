eefea4dffd93d52cebf43ccacf240c94e12452ed
viraj2252 <viraj2252@gmail.com>
2021-12-25 15:24:57 -0500

    Initial files

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<notifications/NotificationsPlugin.h>)
+#import <notifications/NotificationsPlugin.h>
+#else
+@import notifications;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [NotificationsPlugin registerWithRegistrar:[registry registrarForPlugin:@"NotificationsPlugin"]];
+}
+
+@end
---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter_notification_listener/flutter_notification_listener.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: NotificationsLog(),
+    );
+  }
+}
+
+class NotificationsLog extends StatefulWidget {
+  @override
+  _NotificationsLogState createState() => _NotificationsLogState();
+}
+
+class _NotificationsLogState extends State<NotificationsLog> {
+  List<NotificationEvent> _log = [];
+  bool started = false;
+  bool _loading = false;
+
+  ReceivePort port = ReceivePort();
+
+  @override
+  void initState() {
+    initPlatformState();
+    super.initState();
+  }
+
+  // we must use static method, to handle in background
+  static void _callback(NotificationEvent evt) {
+    print("send evt to ui: $evt");
+    final SendPort? send = IsolateNameServer.lookupPortByName("_listener_");
+    if (send == null) print("can't find the sender");
+    send?.send(evt);
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    NotificationsListener.initialize(callbackHandle: _callback);
+
+    // this can fix restart<debug> can't handle error
+    IsolateNameServer.removePortNameMapping("_listener_");
+    IsolateNameServer.registerPortWithName(port.sendPort, "_listener_");
+    port.listen((message) => onData(message));
+
+    // don't use the default receivePort
+    //NotificationsListener.receivePort?.listen((evt) => onData(evt));
+
+    var isR = await NotificationsListener.isRunning;
+    print("""Service is ${!isR! ? "not " : ""}aleary running""");
+
+    setState(() {
+      started = isR;
+    });
+  }
+
+  void onData(NotificationEvent event) {
+    setState(() {
+      _log.add(event);
+    });
+    if (!event.packageName!.contains("example")) {
+      // TODO: fix bug
+      //NotificationsListener.promoteToForeground("");
+    }
+    print(event.toString());
+  }
+
+  void startListening() async {
+    print("start listening");
+    setState(() {
+      _loading = true;
+    });
+    var hasPermission = await NotificationsListener.hasPermission;
+    if (!hasPermission!) {
+      print("no permission, so open settings");
+      NotificationsListener.openPermissionSettings();
+      return;
+    }
+
+    var isR = await NotificationsListener.isRunning;
+
+    if (!isR!) {
+      await NotificationsListener.startService(
+          title: "Listener Running",
+          description: "Let's scrape the notifactions...");
+    }
+
+    setState(() {
+      started = true;
+      _loading = false;
+    });
+  }
+
+  void stopListening() async {
+    print("stop listening");
+
+    setState(() {
+      _loading = true;
+    });
+
+    await NotificationsListener.stopService();
+
+    setState(() {
+      started = false;
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications Listener Example'),
+      ),
+      body: Center(
+          child: ListView.builder(
+              itemCount: _log.length,
+              reverse: true,
+              itemBuilder: (BuildContext context, int idx) {
+                final entry = _log[idx];
+                return ListTile(
+                    trailing:
+                        Text(entry.packageName.toString().split('.').last),
+                    title: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(entry.title ?? "<<no title>>"),
+                          Text(entry.text ?? "<<no text>>"),
+                          Text(entry.createAt.toString().substring(0, 19)),
+                        ],
+                      ),
+                    ));
+              })),
+      floatingActionButton: FloatingActionButton(
+        onPressed: started ? stopListening : startListening,
+        tooltip: 'Start/Stop sensing',
+        child: _loading
+            ? Icon(Icons.close)
+            : (started ? Icon(Icons.stop) : Icon(Icons.play_arrow)),
+      ),
+    );
+  }
+}
